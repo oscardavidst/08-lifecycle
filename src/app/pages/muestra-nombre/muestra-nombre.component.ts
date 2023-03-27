61ab6c4ec66a4536f9c79cef7de330a08e82539b
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-26 22:48:34 -0500

    Carga inicial y creación de los primeros componentes

---
+++ src/app/pages/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [
+  ]
+})
+export class MuestraNombreComponent {
+
+}
---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component {
+
+}
